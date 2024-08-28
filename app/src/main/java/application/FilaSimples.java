80c4a0cac3bd02674148bd03d2c08e61af61dddd
João Gabriel Basso do Prado <gabrielpradowb@hotmail.com>
2024-08-28 01:37:33 +0000

    Fila

---
+++ app/src/main/java/application/FilaSimples.java
@@ -0,0 +1,44 @@
+package application;
+
+public class FilaSimples {
+    //Atributos
+
+    private Integer[] elementos;
+    private int capacidade;
+    private int inicio;
+    private int fim;
+    private int tamanho;
+
+    public FilaSimples(int capacidade){
+        this.capacidade = capacidade;
+        elementos = new Integer[capacidade];
+        inicio = 0;
+        fim = -1;
+        tamanho = 0;
+    }
+//enqueue enfileirar
+    public void enqueue(Integer elemento){
+        if(tamanho == capacidade){
+            throw new IllegalStateException(
+                "A fila está cheia"
+            );
+        }
+        fim = (fim + 1)
+        elementos[fim] = elemento;
+        tamanho++;
+    }
+//desenfileirar dequeue
+    public Integer dequeue() {
+        if(tamanho == 0){
+            throw new IllegalStateException(
+            "A fila está vazia"
+            );
+        }
+        int elementoRemovido= elementos[inicio];
+        inicio = inicio + 1;
+        tamanho--;
+
+        return elementoRemovido;
+
+    }
+}
